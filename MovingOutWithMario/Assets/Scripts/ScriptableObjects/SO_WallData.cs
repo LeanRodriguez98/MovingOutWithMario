c01fbca5342ed41cdc0a638269ec78159cb3675b
Unknown <lean1998@hotmail.com>
2019-01-31 23:55:56 -0300

    TransparentWalls

---
+++ MovingOutWithMario/Assets/Scripts/ScriptableObjects/SO_WallData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Wall Data", menuName = "New Wall Data")]
+public class SO_WallData : ScriptableObject {
+
+    public Color defaultColor;
+    public Color transparentColor;
+}
---
+++ MovingOutWithMario/Assets/Scripts/Wall/Wall.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+    [System.Serializable]
+    public struct WallParts
+    {
+        [HideInInspector] public SpriteRenderer sprite;
+        public SO_WallData wallData;
+    }
+
+    public WallParts[] wallParts;
+
+    // Use this for initialization
+    void Start () {
+        SpriteRenderer[] getSprites = GetComponentsInChildren<SpriteRenderer>();
+        for (int i = 0; i < wallParts.Length; i++)
+        {
+            wallParts[i].sprite = getSprites[i];
+            wallParts[i].sprite.color = wallParts[i].wallData.defaultColor;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerWallTrigger"))
+        {
+            for (int i = 0; i < wallParts.Length; i++)
+            {
+                wallParts[i].sprite.color = wallParts[i].wallData.transparentColor;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerWallTrigger"))
+        {
+            for (int i = 0; i < wallParts.Length; i++)
+            {
+                wallParts[i].sprite.color = wallParts[i].wallData.defaultColor;
+            }
+        }
+    }
+}
